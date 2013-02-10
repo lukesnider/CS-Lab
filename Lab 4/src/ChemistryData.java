b70faa77cee81c48eecf454f5c1d7ebab1634197
Luke Snider <luke946@gmail.com>
2013-02-09 23:26:18 -0800

    Added checkData method

--- Lab 4/src/ChemistryData.java
+++ Lab 4/src/ChemistryData.java
@@ -1,5 +1,6 @@
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
+//import java.io.BufferedInputStream;
+import java.io.EOFException;
+//import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.DataInputStream;
@@ -7,24 +8,30 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ChemistryData {
-	
+
+	final int [] dataMax = {35,55,72,75,45,100};
+	int[][] data = new int[6][10];
 	File readfile;
-	DataInputStream in;
-	String filecontents;
+	DataInputStream in = null;
+	
 	public ChemistryData(String file) {
 		readFile(file);
 	}
 	
 	public void readFile(String file) {
-		int count = 0;
+		
 		try {
 			readfile = new File(file);
-			in = new DataInputStream(new BufferedInputStream(
-                    new FileInputStream(readfile)));
-			while (count < in.available()) {
-				filecontents = filecontents +"\n"+in.readInt();
-			}
+			in = new DataInputStream(new FileInputStream(readfile));
+				for (int r=0;r<6;r++) {
+					for (int c=0;c<10;c++) {
+							data[r][c] = in.readInt();
+					}
+				}
 			in.close();
+		}
+		catch (EOFException e) {
+			System.out.println(e.getMessage());
 		}
 		catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
@@ -34,5 +41,29 @@ public class ChemistryData {
 		}
 		
 		
+	}
+	
+	public boolean checkData(int data, int row) {
+		
+		boolean valid = false;
+		if (row==0 && data<dataMax[0]) {
+			valid = true;
+		}
+		if (row==1 && data<dataMax[1]) {
+			valid = true;
+		}
+		if (row==2 && data<dataMax[2]) {
+			valid = true;
+		}
+		if (row==3 && data<dataMax[3]) {
+			valid = true;
+		}
+		if (row==4 && data<dataMax[4]) {
+			valid = true;
+		}
+		if (row==5 && data<dataMax[5]) {
+			valid = true;
+		}
+		return valid;
 	}
 }
--- Lab 4/src/Lab4.java
+++ Lab 4/src/Lab4.java
@@ -5,14 +5,21 @@ public class Lab4 {
 
 	public static void main(String[] args) {
 		
+		@SuppressWarnings("resource")
 		Scanner keyboard = new Scanner(System.in);
 		
 		System.out.println("Enter a filename ");
 		String file = keyboard.next();
 		ChemistryData string = new ChemistryData(file);
-		System.out.println(string.filecontents);
-		
-		
+
+		int count = 0;
+		for (int r=0;r<6;r++) {
+			for (int c=0;c<10;c++) {
+				System.out.println(string.data[r][c]);
+				count++;
+			}
+		}
+		System.out.println("\n"+count);
 
 	}
 
